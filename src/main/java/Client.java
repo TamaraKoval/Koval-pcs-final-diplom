88b5a5b4d934e2b78b57d1522ad25bee2705a776
TamaraKoval <kovalpsy@mail.ru>
2023-04-08 18:09:07 +0300

    Client file added

---
+++ src/main/java/Client.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    static final int PORT = 8989;
+
+    public static void main(String[] args) {
+
+        String message = "бизнес";
+
+        try (Socket socket = new Socket("localhost", PORT);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            out.println(message);
+            System.out.println(in.readLine());
+
+        } catch (IOException e) {
+            System.out.println("Не могу подключиться к серверу");
+            e.printStackTrace();
+        }
+    }
+}
